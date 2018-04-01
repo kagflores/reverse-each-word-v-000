08503c0cb05c5bc702b651bdb36791b3eb1f0931
Karen Flores <karen.ag.flores@gmail.com>
2018-04-01 03:07:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(sentence)
   #array = []
   array = sentence.split
   reverse_sentence = []
-  array.each do |word|
-    reverse_sentence << word.reverse!
+  array.collect do |word|
+    #reverse_sentence <<
+    word.reverse!
   end
-  return reverse_sentence.join (" ")
+  return array.join (" ")
 end
