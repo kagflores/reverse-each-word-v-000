de0c0fcd82227492ee843382334ee666a70b82b9
Karen Flores <karen.ag.flores@gmail.com>
2018-04-01 03:01:54 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  #array = []
+  array = sentence.split
+  reverse_sentence = []
+  array.each do |word|
+    reverse_sentence << word.reverse!
+  end
+  return reverse_sentence  
+end
